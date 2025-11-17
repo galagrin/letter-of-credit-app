3c426a05a59d6010e532ddef3c675e9a70969f8d
Galina Bobrik <galagrinko@gmail.com>
2025-11-17 10:32:59 +0400

    feat: add put and delete funcs to crudFactory

--- lib/crudFactory.ts
+++ lib/crudFactory.ts
@@ -48,7 +48,7 @@ export function createCrudHandlers<T extends ZodType>({ model, schema, modelName
 
             return NextResponse.json(newItem, { status: 201 });
         } catch (error) {
-            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025") {
                 return NextResponse.json(
                     { error: `${modelName} с такими уникальными полями уже существует` },
                     { status: 409 }
@@ -78,5 +78,88 @@ export function createCrudHandlers<T extends ZodType>({ model, schema, modelName
         }
         return NextResponse.json(items);
     };
-    return { POST, GET };
+
+    // DELETE
+    const DELETE = async (request: Request, { params }: { params: { id: string } }) => {
+        const session = await getServerSession(authOptions);
+        if (!session || session.user.role !== "ADMIN") {
+            return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+        }
+
+        try {
+            const id = parseInt(params.id);
+            switch (model) {
+                case "bank":
+                    await prisma.bank.delete({
+                        where: { id },
+                    });
+                    break;
+                case "company":
+                    await prisma.company.delete({
+                        where: { id },
+                    });
+                    break;
+                default:
+                    throw new Error(`Unsupported model: ${model}`);
+            }
+
+            await prisma.bank.delete({
+                where: { id: parseInt(params.id) },
+            });
+            return new NextResponse(null, { status: 204 }); // 204 No Content
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2025") {
+                return NextResponse.json({ error: `${modelName} с ID ${params.id} не найден` }, { status: 404 });
+            }
+            console.error(`Error creating ${modelName}:`, error);
+            return NextResponse.json({ error: "Произошла ошибка на сервере" }, { status: 500 });
+        }
+    };
+
+    // PUT
+    const PUT = async (request: Request, { params }: { params: { id: string } }) => {
+        const session = await getServerSession(authOptions);
+        if (!session || session.user.role !== "ADMIN") {
+            return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+        }
+        const body = await request.json();
+        const validation = schema.safeParse(body);
+
+        if (!validation.success) {
+            return NextResponse.json(
+                { error: "Неверные данные", details: validation.error.flatten() },
+                { status: 400 }
+            );
+        }
+        try {
+            let updatedItem;
+            switch (model) {
+                case "bank":
+                    updatedItem = await prisma.bank.update({
+                        where: { id: parseInt(params.id) },
+                        data: validation.data as z.infer<T>,
+                    });
+                    break;
+                case "company":
+                    updatedItem = await prisma.company.update({
+                        where: { id: parseInt(params.id) },
+                        data: validation.data as z.infer<T>,
+                    });
+                    break;
+                default:
+                    throw new Error(`Unsupported model: ${model}`);
+            }
+            return NextResponse.json(updatedItem);
+        } catch (error) {
+            if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
+                return NextResponse.json(
+                    { error: `${modelName} с такими уникальными полями уже существует` },
+                    { status: 409 }
+                );
+            }
+            console.error(`Error creating ${modelName}:`, error);
+            return NextResponse.json({ error: "Произошла ошибка на сервере" }, { status: 500 });
+        }
+    };
+    return { POST, GET, DELETE, PUT };
 }
